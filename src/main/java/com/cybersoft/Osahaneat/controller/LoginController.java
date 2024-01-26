c28ae5b411a6277140d5301f4e9bc462b5957ec5
quang <nguyenminhquangquangquang@gmail.com>
2024-01-26 23:59:35 +0700

    Revert "hello testbranch A"
    This reverts commit a9742c07a860303512ccfb3f04bf6ce5e5d4aa56.

--- src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
+++ src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-//hello testbranch
+//hello testbranchAAAAAAAA
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/login")

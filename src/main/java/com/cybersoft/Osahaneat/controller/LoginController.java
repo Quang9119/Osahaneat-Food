2f1a9232da512134afd09d00966290f37e4e6db1
quang <nguyenminhquangquangquang@gmail.com>
2024-01-26 23:57:07 +0700

    push nhầm

--- src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
+++ src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-//hello testbranch
+//hello testbranchAAAAA
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/login")

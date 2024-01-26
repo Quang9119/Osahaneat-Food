a9742c07a860303512ccfb3f04bf6ce5e5d4aa56
quang <nguyenminhquangquangquang@gmail.com>
2024-01-26 23:55:54 +0700

    hello testbranch A

--- src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
+++ src/main/java/com/cybersoft/Osahaneat/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-//hello testbranchAAAAAAAA
+//hello testbranch
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/login")
